426e0cf164395f1eeef7f9a1fa7d5e5bd400a9e3
Mohit Talele <mohittalele@gmail.com>
2021-04-14 06:21:17 +0200

    test some inheritance

--- virtual-function/src/main.cpp
+++ virtual-function/src/main.cpp
@@ -3,8 +3,14 @@
 
 
 using namespace std ;
+class Original{
+	public:
+		void makeNoise(){
+			cout << " make noise in Original class" << endl ;
+		}
+};
 
-class Base {
+class Base : public Original {
 	public:
 		// A virtual function is a member function 
 		// which is declared within a base class and is re-defined(Overriden) by a derived class. 
@@ -14,6 +20,9 @@ class Base {
 		}
 		void print() {
 			cout << " print base class " << endl ;
+		}
+		void speak(){
+			cout << " Speak up in Base class" << endl ;
 		}
 }; 
 
@@ -25,6 +34,9 @@ class Derived : public Base {
 		void print() {
 			cout << " print derived class " << endl ; 
 		}
+		void jump(){
+			cout << " Jump up" << endl ;
+		}
 };	
 
 
@@ -32,14 +44,11 @@ int main(){
 	Base * bptr = NULL ;
 	Derived dclass ;
 	bptr = &dclass ;
-
-	dclass.print() ;
-	
 	bptr->print(); // print() is non virtual function hence compile type binding
-
 	// show() is virtual function (in base class) hence runtime binding. concept: runtime polymorphism
 	// show() will run the actual object's method
 	bptr->show();
-	std::cout << "Hello world!" << std::endl;
+	bptr->speak();
+	bptr->makeNoise();
 	return 0 ;
 }
